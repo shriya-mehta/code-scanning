b3d08835c8e408789cd3e22050bd27cebe82bc55
shriya-mehta <34749378+shriya-mehta@users.noreply.github.com>
2025-03-14 23:16:09 +0530

    Create vuln_app.py

---
+++ vuln_app.py
@@ -0,0 +1,16 @@
+import subprocess, sqlite3
+
+def run_command(cmd):
+    subprocess.run(cmd, shell=True)
+
+def find_user(username):
+    db = sqlite3.connect("users.db")
+    cursor = db.cursor()
+
+    query = f"SELECT * FROM users WHERE name = '{username}'"
+    cursor.execute(query) 
+
+if __name__ == "__main__":
+    user_input = input("Enter a value: ")
+    run_command("echo " + user_input)
+    find_user(user_input)
